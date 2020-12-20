9cfad9b07f78dd42a79d07bcc8c6d16efba79941
dreamienb <dreamienb@gmail.com>
2020-12-20 10:30:31 -0700

    contract v0.1

--- uniswap/LimitOrder.sol
+++ uniswap/LimitOrder.sol
@@ -12,7 +12,7 @@ contract LimitSwap{
     address public uniswapRouterAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     IUniswapV2Router uniswapRouter;
     
-    mapping(uint64 => address) requester;
+    mapping(uint64 => address payable) requester;
     mapping(uint64 => address) inaddress;
     mapping(uint64 => address) outaddress;
     mapping(uint64 => uint256) inamt;
@@ -28,6 +28,9 @@ contract LimitSwap{
     receive() external payable {
         revert(); //revert mistaken eth
     }
+    /**
+	 * @dev adds inamount to paymentBalance, so that eth given to contract, but not fulfilled, can be refunded
+	 * */
 	function requestSwapExactTokenForTokens(address intoken, address outtoken, uint256 inamount, uint256 minoutamount, uint256 expiretime) 
 	external payable{
 	    require(msg.value == flatcost, 'invalid payment');
@@ -47,6 +50,9 @@ contract LimitSwap{
 	    it.approve(uniswapRouterAddress, inamount); //approve uniswap router to spend tokens
 	    emit Requested(currentid, msg.sender, inamt[currentid], outamt[currentid], expiretime, outexact[currentid]);
 	}
+	/**
+	 * @dev adds inamount to paymentBalance, so that eth given to contract, but not fulfilled, can be refunded
+	 * */
 	function requestSwapTokenForExactTokens(address intoken, address outtoken, uint256 inamount, uint256 outamount, uint256 expiretime) 
 	external payable{
 	    require(msg.value == flatcost, 'invalid payment');
@@ -62,8 +68,8 @@ contract LimitSwap{
 	    outexact[currentid] = true;
 	    
 	    ERC20 it = ERC20(intoken);
-	    it.transferFrom(msg.sender, address(this), inamount); //transfer tokens from user to this
-	    it.approve(uniswapRouterAddress, inamount); //approve uniswap router to spend tokens
+	    //it.transferFrom(msg.sender, address(this), inamount); //transfer tokens from user to this
+	    //it.approve(uniswapRouterAddress, inamount); //approve uniswap router to spend tokens
 	    emit Requested(currentid, msg.sender, inamt[currentid], outamt[currentid], expiretime, outexact[currentid]);
 	}
 	/**
@@ -137,40 +143,44 @@ contract LimitSwap{
         require(id <= currentid, 'invalid id');
         
         if(inaddress[id] == address(0)){
-            if(outexact[id]){
-                uniswapRouter.swapETHForExactTokens{value: inamt[id]}(
-                    outamt[id], path, requester[id], deadline[id]
-                    );
-            }else{
-               uniswapRouter.swapExactETHForTokens{value: inamt[id]}(
-                   outamt[id], path, requester[id], deadline[id]
-                   ); 
-            }
+            uniswapRouter.swapETHForExactTokens{value: inamt[id]}(
+                outamt[id], path, requester[id], deadline[id]
+                );
+            ERC20 t = ERC20(path[path.length - 1]);
+            t.transferFrom(address(this), requester[id], outamt[id]);
         } else if(outaddress[id] == address(0)){
-            if(outexact[id]){
-                uniswapRouter.swapTokensForExactETH(
-                    outamt[id], inamt[id], path, requester[id], deadline[id]
-                    );
-            }else{
-                uniswapRouter.swapExactTokensForETH(
-                    inamt[id], outamt[id], path, requester[id], deadline[id]
-                    );
-            }
+            uniswapRouter.swapTokensForExactETH(
+                outamt[id], inamt[id], path, requester[id], deadline[id]
+                );
+            requester[id].transfer(outamt[id]);
         } else {
             require(path[0] == inaddress[id] && path[path.length - 1] == outaddress[id], 'invalid path');
-            if(outexact[id]){
-                uniswapRouter.swapTokensForExactTokens(
-                    inamt[id], outamt[id], path, requester[id], deadline[id]
-                    );
-            }else{
-                uniswapRouter.swapExactTokensForTokens(
-                    outamt[id], inamt[id], path, requester[id], deadline[id]
-                    );
+            uniswapRouter.swapTokensForExactTokens(
+                inamt[id], outamt[id], path, requester[id], deadline[id]
+                );
+            ERC20 t = ERC20(path[path.length - 1]);
+            t.transferFrom(address(this), requester[id], outamt[id]);
+        }
+        emit Fulfilled(id, msg.sender,  outamt[id], path);
+    }
+    function batchRefundTokens(uint64[] memory ids) external {
+        address tokenaddress = outaddress[ids[0]];
+        uint256 refund;
+        uint64[] memory successfulRefunds = new uint64[](ids.length);
+        for(uint i = 0; i < ids.length; i++){
+            require(requester[ids[i]] == msg.sender || admin == msg.sender);
+            require(outaddress[ids[i]] == tokenaddress, "You may only batch refunds for the same token address");
+            if(paymentBalance[msg.sender][ids[i]] > 0){
+                successfulRefunds[i] = ids[i];
+                refund += paymentBalance[msg.sender][ids[i]];
+                paymentBalance[msg.sender][ids[i]] = 0;
             }
         }
-        emit Fulfilled(id, msg.sender, path);
+        ERC20 t = ERC20(tokenaddress);
+        t.transferFrom(address(this), msg.sender, refund);
+        emit Refunded(successfulRefunds);
     }
-    function batchRefund(uint64[] memory ids) external {
+    function batchRefundEth(uint64[] memory ids) external {
         uint256 refund;
         uint64[] memory successfulRefunds = new uint64[](ids.length);
         for(uint i = 0; i < ids.length; i++){
@@ -204,5 +214,5 @@ contract LimitSwap{
     }
     event Refunded(uint64[] ids);
     event Requested(uint64 indexed id, address indexed requester, uint256 inamt, uint256 outamt, uint256 deadline, bool outexact);
-    event Fulfilled(uint64 indexed id, address indexed fulfiller, address[] path);
+    event Fulfilled(uint64 indexed id, address indexed fulfiller, uint256 outamt, address[] path);
 }
